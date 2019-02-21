3efd7d171f0d6a4e2408a9fc92edd84a5077d350
nenadmudri <43782866+nenadmudri@users.noreply.github.com>
2019-02-21 11:49:22 +0100

    Update CurrencyExchangeServiceProxy.java

--- src/main/java/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
+++ src/main/java/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="forex-service")
+@FeignClient(name="forex-service", url="localhost:8000")
 @RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
